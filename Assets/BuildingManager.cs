c32d6e5dfbd4126929288f38b2a044ff1f699147
Michał Ślusarski <115535646+MichalMSlusarski@users.noreply.github.com>
2023-09-30 16:50:05 +0200

    Restore

---
+++ Assets/BuildingManager.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BuildingManager : MonoBehaviour
+{
+    public GameObject[] buildingBlocks;
+
+    public LayerMask layerMask;
+
+    public int selectedBuilding = 0;
+
+    public bool isInBuildMode = false;
+
+    public Material mockMaterial;
+
+    private void Update()
+    {
+        MouseOverCheck();
+    }
+
+    private void MouseOverCheck()
+    {
+        if (!isInBuildMode) { return; }
+
+        Ray ray;
+        RaycastHit hit;
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition); //Touch touch = Input.GetTouch(0); ray = Camera.main.ScreenPointToRay(touch.positon);  
+
+        if (Physics.Raycast(ray, out hit, 10000f, layerMask))
+        {
+            if (hit.transform.CompareTag("Tile"))
+            {
+                PlaceMockBuilding(hit.transform.gameObject);
+            }
+        }
+    }
+
+    private void PlaceMockBuilding(GameObject tile)
+    {
+        if (tile.transform.childCount > 0) { return; }
+
+        Vector3 position = new Vector3(tile.transform.position.x, tile.transform.position.y + 0.501f, tile.transform.position.z);
+
+        GameObject mockBuilding = Instantiate(buildingBlocks[selectedBuilding], position, Quaternion.identity);
+        mockBuilding.GetComponent<Rigidbody>().isKinematic = true;
+        mockBuilding.transform.SetParent(tile.transform);
+        mockBuilding.GetComponent<MeshRenderer>().material = mockMaterial;
+        Destroy(mockBuilding, 0.15f);
+
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            GameObject building = Instantiate(buildingBlocks[selectedBuilding], position, Quaternion.identity);
+            building.GetComponent<Rigidbody>().isKinematic = true;
+            Destroy(mockBuilding);
+        }
+    }
+}
